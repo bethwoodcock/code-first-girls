2fb3c4c6e295c6db4bfa42237cda895a34fd5c8a
Beth Woodcock <108340152+bethwoodcock@users.noreply.github.com>
2022-07-12 20:25:49 +0100

    Data Types and Variables

---
+++ main.py
@@ -0,0 +1,12 @@
+cats = 10
+cans_per_day = 7
+
+total_cans = cats * cans_per_day
+
+output = '{} cats need {} cans per day, which is {} cans'.format(cats, cans_per_day, total_cans)
+print(output)
+
+
+
+
+
